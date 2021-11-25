84b7b766406b051e268e069cc918a1dc2956eb82
Embdsys <86981025+Embdsys@users.noreply.github.com>
2021-11-24 21:54:21 -0700

    First commit
    main works and checks if camera is occupied

---
+++ main.py
@@ -0,0 +1,11 @@
+import MotionDetector1
+import ColorDetect
+#import CameraStatus
+
+# Still shows windows for demonstration
+
+# Press q to exit the windows, represents accelerometer
+#MotionDetector1.DetectMotion()
+
+# Color detect for camera obstruction
+ColorDetect.DetectColors()
